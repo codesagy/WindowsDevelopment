bf41e28be04b34eec9db332410d37977796e8863
Sagar Adhalge <codesagy@gmail.com>
2020-09-02 20:02:54 +0530

    Create FirstWindow.cpp
    First window creatiion program.
    SUBSYSTEM : Windows

---
+++ First Window/FirstWindow.cpp
@@ -0,0 +1,66 @@
+#include <Windows.h>
+
+// Function Declarations
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+// Entry Point Function
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpszCmdLine, int nCmdShow)
+{
+	// Local Variables
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	char AppName[] = "Windows Program";
+
+	// Code
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.lpszClassName = AppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	// Registration
+	RegisterClassEx(&wndclass);
+	hwnd = CreateWindow(AppName, "Sagar's First Window",
+		WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
+
+	// Displaying of Window
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	// Message Loop
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (msg.wParam);
+} // End of WinMain()
+
+// Window Procedure (Window Function)
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	// Code
+	switch (iMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	return 0;
+
+	}
+	return (DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+//End of Program
